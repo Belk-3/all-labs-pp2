1bd9db367c8e6d6165312a0ff8f948336f145e65
Belka <bekzhan200707@icloud.com>
2025-01-29 15:41:01 +0500

    re save

--- labs_pp2/lab1/py4_Syntax/4.py
+++ labs_pp2/lab1/py4_Syntax/4.py
@@ -1,2 +1,12 @@
 if 9 > 2:
     print("nine is greater than two !")
+
+#do not skip tab ! and use same number of space to same code block or you again get eror
+#----------------------------------------------------------------------------------------------    
+# variable
+    x = 10
+    y = "Me"
+#----------------------------------------------------------------------------------------------
+# for comment use "#" then you can write anything
+# this is a comment
+#----------------------------------------------------------------------------------------------
---
+++ labs_pp2/lab1/py5_Comments/5.py
@@ -0,0 +1,3 @@
+#comments like this is nothing and can not to affect to the code. I think here a lot of mistakes xD
+#print("Hello world")
+print("Hello Lecture")
