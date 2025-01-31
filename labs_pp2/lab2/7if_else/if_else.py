fdb0b17395d8000e828367789ba08187190f8ee6
Bekzhan Myrzabekov <b_myrzabekov@kbtu.kz>
2025-01-31 18:06:58 +0500

    d

--- labs_pp2/lab2/7if_else/if_else.py
+++ labs_pp2/lab2/7if_else/if_else.py
@@ -1,6 +1,7 @@
 #here i will write things that i dont know
 #Short Hand If
 #If you have only one statement to execute, you can put it on the same line as the if statement.
-
+a = 5
+b = 2
 if a > b: print("a is greater than b")
 
